dc6fa48266cb1be7841089a822f5d40b61a307c7
binoyam <btechan@gmail.com>
2023-08-08 05:17:59 +0300

    ...

---
+++ script.js
@@ -0,0 +1,13 @@
+const smallimges = document.querySelectorAll(".thumbnail");
+
+
+/* figure out how to change the displayed picture 
+ when small image is clicked on,
+ maybe array of src for the large images..?
+ or give each small image a unique class name and use 
+ if statement to change the src of the displayable image*/
+ 
+ /* start with second option*/
+ 
+ /* create a class style that gives the thumbnail images
+ a blur effect when they are active*/
