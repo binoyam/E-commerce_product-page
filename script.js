459650ed83504d579ee98ddce3b0ca8c4c56a83b
binoyam <btechan@gmail.com>
2023-08-09 04:35:58 +0300

    notification and qnatity selector

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const smallimges = document.querySelectorAll(".thumbnail");
-const productDisplay = document.querySelector(".preview");
-const cartBtn = document.querySelector(".cart");
-const cart_content = document.querySelector(".cart-div");
+/* image switcher variables*/
+const smallimges = document.querySelectorAll(".thumbnail");// small thubnails
+const productDisplay = document.querySelector(".preview"); //big image
 
-cartBtn.addEventListener('click', showCart);
+/* cart variables*/
+const cartBtn = document.querySelector(".cart"); //cart icon
+const cart_content = document.querySelector(".cart-div"); //cart dropdown
+const cartNotification = document.querySelector(".notification");//notification
 
-function showCart(){
+/* product quantity selection and add to cart variables*/
+const minusBtn = document.querySelector(".minus"); //minus sign btn
+const plusBtn = document.querySelector(".plus"); //plus sign btn
+const addToCartBtn = document.querySelector(".addtocart");// add to cart button
+const prodQuantity = document.querySelector(".quantity");// quantity amount
+let counter = 0;//for current quantity display
+
+/* function: show/ hide cart dropdown */
+cartBtn.addEventListener('click', () => {
     cart_content.classList.toggle("active-cart");
-}
+    if (cartNotification.innerText) {
+        const message = document.createElement("P");
+        const text = document.createTextNode("Your cart is empty.");
+        message.appendChild(text);
+        message.style.color = "var(--Darkgrayishblue)";
+        cart_content.appendChild(message);
+    }
+});
 
+/* function: display the large image of the thumbnail clicked */
 smallimges.forEach((smlimg) => {
     smlimg.addEventListener('click', () => {
         smallimges.forEach((img) => {
@@ -32,3 +50,29 @@ smallimges.forEach((smlimg) => {
         }
     });
 });
+/* function: update qunatity */
+plusBtn.addEventListener('click', incrementQuantity);
+minusBtn.addEventListener('click', decrementQuantity)
+
+function incrementQuantity() {
+    counter++;
+    prodQuantity.innerText = counter;
+}
+function decrementQuantity() {
+    if (counter > 0) {
+        counter--;
+        prodQuantity.innerText = counter;
+    }
+}
+/* function: add to cart */
+addToCartBtn.addEventListener('click', addToCart);
+
+function addToCart() {
+    if (prodQuantity.innerText > 0) {
+        cartNotification.classList.add("notify");
+        cartNotification.innerText = prodQuantity.innerText;
+    }
+    else {
+        cartNotification.classList.remove("notify");
+    }
+}
