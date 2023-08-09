2988e228077ec5a7d5e07117ed89cd2bbbdb3eb7
binoyam <btechan@gmail.com>
2023-08-10 02:03:45 +0300

    made lighbox thumbnails interactive

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 /* image switcher variables*/
-const smallimges = document.querySelectorAll(".thumbnail");// small thubnails
+const thumbnails = document.querySelectorAll(".thumbnail");// small thumbnails
 const productDisplay = document.querySelector(".preview"); //big image
 
 /* cart variables*/
-const cartBtn = document.querySelector(".cart"); //cart icon
+const cartIcon = document.querySelector(".cart"); //cart icon
 const cart_content = document.querySelector(".cart-div"); //cart dropdown
 const cartNotification = document.querySelector(".notification");//notification
 const cartBox = document.querySelector(".cart-box"); // cart content holder
-const cartEmptyMessage = document.querySelector(".zero");// your cart is empty message
-const deleteCart = document.querySelector(".delete");// delete buttton inside cart
+const cartEmptyMessage = document.querySelector(".zero");// "your cart is empty" message
 
 /* product quantity selection and add to cart variables*/
 const minusBtn = document.querySelector(".minus"); //minus sign btn
@@ -17,15 +16,26 @@ const addToCartBtn = document.querySelector(".addtocart");// add to cart button
 const prodQuantity = document.querySelector(".quantity");// quantity amount
 let counter = 0;//for current quantity display
 
+/* lightbox effect varialbes*/
+const lightBox = document.querySelector(".light-box-div");//lightbox container
+const closeBtn = document.querySelector(".close");// close lightbox icon
+const previousBtn = document.querySelector(".previous-btn");// left arrow
+const nextBtn = document.querySelector(".next-btn");// right arrow
+const lightboxDisplay = document.querySelector(".lightbox-preview")//lightbox big image
+const lightBoxThumbs = document.querySelectorAll(".thumbnail");//lightbox thumbnails//maynot be unneccessary?
+
+
 /* function: show/ hide cart dropdown */
-cartBtn.addEventListener('click', () => {
+cartIcon.addEventListener('click', () => {
     cart_content.classList.toggle("active-cart");
 });
 
-/* function: display the large image of the thumbnail clicked */
-smallimges.forEach((smlimg) => {
+/* function: display the large image of the thumbnail clicked 
+        for both normal and lightbox*/
+
+thumbnails.forEach((smlimg) => {
     smlimg.addEventListener('click', () => {
-        smallimges.forEach((img) => {
+        thumbnails.forEach((img) => {
             img.classList.remove("active");
         });
         smlimg.classList.add("active");
@@ -33,20 +43,26 @@ smallimges.forEach((smlimg) => {
         switch (activeImg.getAttribute("src")) {
             case "./images/image-product-1-thumbnail.jpg":
                 productDisplay.src = "./images/image-product-1.jpg";
+                lightboxDisplay.src = "./images/image-product-1.jpg";
                 break;
             case "./images/image-product-2-thumbnail.jpg":
                 productDisplay.src = "./images/image-product-2.jpg";
+                lightboxDisplay.src = "./images/image-product-2.jpg";
                 break;
             case "./images/image-product-3-thumbnail.jpg":
                 productDisplay.src = "./images/image-product-3.jpg";
+                lightboxDisplay.src = "./images/image-product-3.jpg";
                 break;
             case "./images/image-product-4-thumbnail.jpg":
                 productDisplay.src = "./images/image-product-4.jpg";
+                lightboxDisplay.src = "./images/image-product-4.jpg";
                 break;
         }
     });
 });
+
 /* function: update qunatity */
+
 plusBtn.addEventListener('click', incrementQuantity);
 minusBtn.addEventListener('click', decrementQuantity)
 
@@ -60,7 +76,9 @@ function decrementQuantity() {
         prodQuantity.innerText = counter;
     }
 }
+
 /* function: add to cart */
+
 addToCartBtn.addEventListener('click', addToCart);
 
 function addToCart() {
@@ -68,6 +86,8 @@ function addToCart() {
         cartEmptyMessage.style.display = "none";
         const total = 125 * counter;
         cartBox.style.display = "flex";
+        cartNotification.classList.add("notify");
+        cartNotification.innerText = counter;
 
         cartBox.innerHTML = `<div class="cart-content">
         <img class="prd" src="./images/image-product-1-thumbnail.jpg" alt="product1">
@@ -77,15 +97,12 @@ function addToCart() {
             <span class="price">$125.00</span>
             <span class="times">&times;</span>
             <span class="amount">${counter}</span>
-            <span class="total">$${total}</span>
+            <span class="total">$${total}.00</span>
           </p>
         </div>
         <button class="delete" onClick="removeCart()"><img src="./images/icon-delete.svg" alt="icon-delete"></button>
       </div>
       <button class="checkout" onClick="checkout()">Checkout</button>`;
-
-        cartNotification.classList.add("notify");
-        cartNotification.innerText = counter;
     }
     else {
         cartEmptyMessage.style.display = "block";
@@ -95,13 +112,17 @@ function addToCart() {
 }
 
 /*function: remove cart contents*/
+
 function removeCart() {
     // counter = 0;
     cartEmptyMessage.style.display = "block";
     cartBox.style.display = "none";
     cartNotification.classList.remove("notify");
 }
-
+/* function: open lightbox */
+/*function: close lightbox effect */
+/* function: display previous image */
+/* function: display next image */
 
 
 
