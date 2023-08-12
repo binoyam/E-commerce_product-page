1e1fef72b602017e98433932d675e49ea6146fc5
binoyam <btechan@gmail.com>
2023-08-13 01:38:22 +0300

    done

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Frontend Mentor challenge - E-commerce product page
-## 
+## [Preview](https://64d8056917608f4242f159f5--stirring-dango-b2fae2.netlify.app)
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-/* alteranative for line:40 - 70*/ 
+/* alteranative code for line:40 - 70*/ 
+
 /* function: display the large image of the thumbnail clicked 
         for both normal and lightbox*/
-
 let activeThumbnail = null;
 
 function updateDisplayedImage(imageUrl) {
